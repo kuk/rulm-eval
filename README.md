56ff15b44549de3deb00daecf942caf90cc25b96
Alexander Kukushkin <alex@alexkuk.ru>
2023-03-11 20:20:09 +0300

    Stage 01 02

---
+++ README.md
@@ -0,0 +1,37 @@
+
+Dev env
+
+```bash
+
+python -m venv ~/.venvs/rulm-eval
+source ~/.venvs/rulm-eval/bin/activate
+
+pip install -r requirements.txt
+
+pip install ipykernel
+python -m ipykernel install --user --name rulm-eval
+```
+
+Data
+
+```
+mkdir data
+
+# rsg
+wget --no-check-certificate https://russiansuperglue.com/tasks/download
+unzip download
+rm -r download __MACOSX
+mv combined data/rsg
+
+# rucola
+mkdir data/rucola
+wget https://github.com/RussianNLP/RuCoLA/raw/main/data/in_domain_dev.csv -P data/rucola
+wget https://github.com/RussianNLP/RuCoLA/raw/main/data/out_of_domain_dev.csv -P data/rucola
+```
+
+Evals
+
+```
+01_openai_terra TERRA_PROMPT_V1 TEXT_DAVINCHI_003
+02_openai_danetqa DANETQA_PROMPT_V1 TEXT_DAVINCHI_003
+```
---
+++ main.py
@@ -0,0 +1,280 @@
+
+import os
+import sys
+import csv
+import json
+import random
+from time import sleep
+from pathlib import Path
+from collections import Counter
+
+import requests
+
+from tqdm import tqdm as log_progress
+
+
+########
+#
+#   CONST
+#
+#####
+
+TERRA = 'terra'
+DANETQA = 'danetqa'
+
+OPENAI_TOKEN = os.getenv('OPENAI_TOKEN')
+
+TEXT_DAVINCHI_003 = 'text-davinci-003'
+CODE_DAVINCHI_002 = 'code-davinci-002'
+GPT_35_TURBO_0301 = 'gpt-3.5-turbo-0301'
+
+
+#######
+#
+#   LINES
+#
+######
+
+
+def read_lines(path):
+    with open(path) as file:
+        for line in file:
+            yield line.rstrip('\n')
+
+
+def write_lines(path, lines):
+    with open(path, 'w') as file:
+        for line in lines:
+            file.write(line + '\n')
+
+
+#######
+#
+#   JSONL
+#
+#####
+
+
+def parse_jsonl(lines):
+    for line in lines:
+        yield json.loads(line)
+
+
+def format_jsonl(items):
+    for item in items:
+        yield json.dumps(item, ensure_ascii=False, indent=None)
+
+
+#######
+#
+#  CSV
+#
+######
+
+
+def read_csv(path):
+    with open(path, newline='') as file:
+        yield from csv.DictReader(file)
+
+
+########
+#
+#   DOTENV
+#
+######
+
+
+DOTENV_PATH = '.env'
+
+
+def parse_dotenv(lines):
+    for line in lines:
+        if line:
+            key, value = line.split('=', 1)
+            yield key, value
+
+
+#######
+#
+#  TERRA
+#
+#####
+
+
+# {'premise': '"По словам россиянина, перед ним стояла задача - финишировать впереди ""Форс Индии"". ""Мы начали гонку на покрышках средней жесткости. И я старался отстоять свою позицию на старте, так как все в основном были на мягких шинах""."',
+#  'hypothesis': 'Соперники выступали преимущественно на мягких шинах.',
+#  'label': 'entailment',
+#  'idx': 104}
+
+
+TERRA_PROMPT_V1 = '''
+Does Premise entail Hypothesis? Choose Yes or No.
+
+Premise: Трижды он был привлечён судебным приставом к административной ответственности по ст. 17.15 КоАП РФ за неисполнение содержащихся в исполнительном документе требований неимущественного характера. Так как срок для добровольного исполнения истёк, пристрой снесли принудительно.
+Hypothesis: Пристрой был снесен.
+Entail: Yes
+
+Premise: Для молодого организма это не прошло бесследно. Резкое токсическое воздействие этанола привело к смерти парня. Его тело обнаружила бабушка, которая вернулась на следующий день.
+Hypothesis: Молодой организм стал сильнее от этанола.
+Entail: No
+
+Premise: {premise}
+Hypothesis: {hypothesis}
+Entail: 
+'''
+
+
+def terra_prompt(item, template=TERRA_PROMPT_V1):
+    return template.format(
+        premise=item['premise'],
+        hypothesis=item['hypothesis']
+    )
+
+
+def norm_terra_response(response):
+    if 'Yes' in response:
+        return 'entailment'
+    elif 'No' in response:
+        return 'not_entailment'
+
+
+######
+#
+#   DANETQA
+#
+######
+
+
+# {'question': 'Есть ли вода на марсе?',
+#  'passage': 'Гидросфера Марса — это совокупность водных запасов планеты Марс, представленная водным льдом в полярных шапках Марса, льдом над поверхностью, сезонными ручьями из жидкой воды и возможными резервуарами жидкой воды и водных растворов солей в верхних слоях литосферы Марса. Гидросфера ... е шапки Марса, так как предполагалось, что они могут состоять из водного льда по аналогии с Антарктидой или Гренландией на Земле, однако высказывалась и гипотеза, что это твёрдый диоксид углерода.',
+#  'label': True,
+
+
+DANETQA_PROMPT_V1 = '''
+Given Passage answer the Question. Choose Yes or No.
+
+Passage: Пётр Моисеевич Миронов  — красноармеец Рабоче-крестьянской Красной Армии, участник Великой Отечественной войны, Герой Советского Союза . Пётр Миронов родился в 1904 году в деревне Утринка . После окончания шести классов школы проживал в Москве, работал в сфере общепита. В июне 1941 года Миронов был призван на службу в Рабоче-крестьянскую Красную Армию. С июля 1942 года — на фронтах Великой Отечественной войны.
+Question: Был ли миронов в армии?
+Answer: Yes
+
+Passage: Брюс Ли  — гонконгский и американский киноактёр, режиссёр, сценарист, продюсер, популяризатор и реформатор в области китайских боевых искусств, мастер боевых искусств, постановщик боевых сцен и философ, основоположник стиля Джит Кун-До. Брюс Ли начал сниматься в кино с детства. Его детское имя — Ли Сяолун , взрослое имя — Ли Чжэньфань .
+Question: Правда ли что брюс ли не был бойцом?
+Answer: No
+
+Passage: {passage}
+Question: {question}
+Answer: 
+'''
+
+
+def danetqa_prompt(item, template=DANETQA_PROMPT_V1):
+    return template.format(
+        passage=item['passage'],
+        question=item['question']
+    )
+
+
+def norm_danetqa_response(response):
+    if 'Yes' in response:
+        return True
+    elif 'No' in response:
+        return False
+
+
+#####
+#
+#   NORM RESP
+#
+###
+
+
+NORM_RESPONSE = {
+    TERRA: norm_terra_response,
+    DANETQA: norm_danetqa_response
+}
+
+
+######
+#
+#   SCORE
+#
+#####
+
+
+def acc_score(id_targets, id_preds):
+    total = 0
+    acc = 0
+    for id in id_targets.keys() | id_preds.keys():
+        total += 1
+        acc += id_targets.get(id) == id_preds.get(id)
+    return acc / total
+
+
+########
+#
+#   OPENAI
+#
+######
+
+
+def parse_openai_stream(lines):
+    for line in lines:
+        if line.startswith(b'data: '):
+            line = line[len('data: '):]
+            if line == b'[DONE]':
+                break
+            yield json.loads(line)
+
+
+def post_openai_stream(url, payload, token):
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {token}'
+    }
+
+    with requests.post(
+        url,
+        json=payload,
+        headers=headers,
+        stream=True
+    ) as response:
+        response.raise_for_status()
+        lines = response.iter_lines()
+        yield from parse_openai_stream(lines)
+
+
+def openai_generate_stream(
+        prompt,
+        model=TEXT_DAVINCHI_003, max_tokens=128,
+        temperature=0, top_p=1,
+        token=OPENAI_TOKEN
+):
+    items = post_openai_stream(
+        'https://api.openai.com/v1/completions',
+        {
+            'prompt': prompt,
+            'model': model,
+            'max_tokens': max_tokens,
+            'temperature': temperature,
+            'top_p': top_p,
+            'stream': True
+        },
+        token
+    )
+    for item in items:
+        yield item['choices'][0]['text']
+
+
+def join_print_tokens(tokens):
+    buffer = []
+
+    for token in tokens:
+        buffer.append(token)
+        sys.stdout.write(token)
+
+    sys.stdout.flush()
+    return ''.join(buffer)
+
+
+def join_tokens(tokens):
+    return ''.join(tokens)
